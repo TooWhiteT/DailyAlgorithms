81402ce0bded60d518e98bf8c47f0f15515e4619
TooWhite <toowhitedog@outlook.com>
2022-10-14 15:40:24 +0800

    每日算法

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,4 @@
 // 验证
 fun main(args: Array<String>) {
-    val arr = intArrayOf(0, 0, 1, 1, 1, 2, 2, 3, 3, 4)
-    val size = removeDuplicates(arr)
-    arr.copyOfRange(0, size).forEach {
-        println(it)
-    }
+
 }
--- src/main/kotlin/Primary.kt
+++ src/main/kotlin/Primary.kt
@@ -35,3 +35,79 @@ fun maxProfit(prices: IntArray): Int {
     }
     return profit
 }
+
+/**
+ * 旋转数组
+ *
+ * 给你一个数组，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+ * @see <a href="https://leetcode.cn/leetbook/read/top-interview-questions-easy/x2skh7/">旋转数组</a>
+ */
+fun rotate(nums: IntArray, k: Int) {
+    val temp = IntArray(nums.size)
+    for (i in 0 until nums.size) {
+        temp[i] = nums[i]
+    }
+    for (j in 0 until temp.size) {
+        // (j + k) % nums.size 偏移后的下标
+        nums[(j + k) % nums.size] = temp[j]
+    }
+}
+
+// 空间复杂度O(1)的解法 翻转数组
+fun rotate2(nums: IntArray, k: Int) {
+    if (nums.size < 2 || k <= 0) return
+    nums.reverse()
+    nums.reverse(0, k)
+    nums.reverse(k, nums.size)
+}
+
+/**
+ * 存在重复元素
+ *
+ * 给你一个整数数组 nums 。如果任一值在数组中出现 至少两次 ，返回 true ；如果数组中每个元素互不相同，返回 false 。
+ *
+ * @see <a href="https://leetcode.cn/leetbook/read/top-interview-questions-easy/x248f5/">存在重复元素</a>
+ */
+fun containsDuplicate(nums: IntArray): Boolean {
+    val temp = HashSet<Int>()
+    for (i in nums) {
+        if (!temp.add(i)) {
+            return true
+        }
+    }
+    return false
+}
+
+/**
+ * 只出现一次的数字
+ *
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ *
+ * @see <a href="https://leetcode.cn/leetbook/read/top-interview-questions-easy/x21ib6/">只出现一次的数字</a>
+ */
+fun singleNumber(nums: IntArray): Int {
+    val temp = HashSet<Int>()
+    for (i in nums) {
+        if (!temp.add(i)) { // Set特性 无法添加重复值
+            temp.remove(i)
+        }
+    }
+    return temp.first()
+}
+
+/**
+ * 使用位运算符(异或)
+ *
+ * 使用异或运算，将所有值进行异或
+ *
+ * 异或运算，相异为真，相同为假，所以 a^a = 0 ; 0^a = a 相同为0 不同返回原值
+ *
+ * 因为异或运算 满足交换律 a^b^a = a^a^b = b 所以数组经过异或运算，单独的值就剩下了
+ */
+fun singleNumber2(nums: IntArray): Int {
+    var temp: Int = 0;
+    for (i in nums) {
+        temp = temp xor i
+    }
+    return temp
+}
