b9a07cc6e69fdcfb0efa8c057730e17007b295a5
TooWhiteT <tzg1064690854@gmail.com>
2022-10-23 18:19:29 +0800

    每日算法 罗马数字转整数

--- src/main/kotlin/Primary.kt
+++ src/main/kotlin/Primary.kt
@@ -1,5 +1,7 @@
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
+import kotlin.collections.MutableMap as MutableMap
 
 /**
  * 删除排序数组中的重复项
@@ -1228,7 +1230,6 @@ class Solution(val nums: IntArray) {
  *
  * @see <a href="https://leetcode.cn/leetbook/read/top-interview-questions-easy/xnkq37/"></a>
  */
-
 data class StackNode(val `val`: Int, val min: Int)
 class MinStack() {
     val stack = Stack<StackNode>()
@@ -1260,4 +1261,145 @@ class MinStack() {
         }
         return stack.peek().min
     }
+}
+
+/**
+ * Fizz Buzz
+ *
+ * 给你一个整数 n ，找出从 1 到 n 各个整数的 Fizz Buzz 表示，并用字符串数组 answer（下标从 1 开始）返回结果，其中：
+ * answer`[i]` == "FizzBuzz" 如果 i 同时是 3 和 5 的倍数。
+ * answer`[i]` == "Fizz" 如果 i 是 3 的倍数。
+ * answer`[i]` == "Buzz" 如果 i 是 5 的倍数。
+ * answer`[i]` == i （以字符串形式）如果上述条件全不满足。
+ *
+ * @see <a href="https://leetcode.cn/leetbook/read/top-interview-questions-easy/xngt85/">Fizz Buzz</a>
+ */
+fun fizzBuzz(n: Int): List<String> {
+    val res = ArrayList<String>()
+    for (i in 1 .. n) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            res.add("FizzBuzz")
+            continue
+        } else if (i % 3 == 0 ) {
+            res.add("Fizz")
+            continue
+        } else if (i % 5 == 0) {
+            res.add("Buzz")
+            continue
+        } else {
+            res.add("$i")
+            continue
+        }
+    }
+    return res
+}
+
+/**
+ * 计数质数
+ *
+ * 给定整数 n ，返回 所有小于非负整数 n 的质数的数量 。
+ *
+ * @see <a href="https://leetcode.cn/leetbook/read/top-interview-questions-easy/xnzlu6/">计数质数</a>
+ */
+fun countPrimes(n: Int): Int {
+    // 埃氏筛
+    // 初始化只是质数数组,并填写默认值为1
+    var primes = IntArray(n,{1})
+    // 计数器
+    var count = 0
+    var i = 2
+    var j = 0L
+    // 1 既不是质数也不是合数，所以从2开始
+    while(i < n){
+        if(primes[i] == 1){
+            count++
+            // 从 i*i 开始，所有i的倍数，都置为合数，即0
+            // i*i 开始防止重复，比如 2*6 和 6*2
+            // i的倍数一定是合数，j += i 就等于i的倍数在递增
+            // 此处注意，i*i 可能会超过Int的MAX_VALUE，所以先转成Long。
+            j = i.toLong() * i.toLong()
+            while(j < n){
+                // 作为下标，再转回来Int。
+                primes[j.toInt()] = 0
+                j+=i
+            }
+        }
+        i++
+    }
+    return count
+}
+
+/**
+ * 3的幂
+ *
+ * 给定一个整数，写一个函数来判断它是否是 3 的幂次方。如果是，返回 true ；否则，返回 false 。
+ *
+ * 整数 n 是 3 的幂次方需满足：存在整数 x 使得 n == 3^x
+ *
+ * @see <a href="https://leetcode.cn/leetbook/read/top-interview-questions-easy/xnsdi2/">3的幂</a>
+ */
+fun isPowerOfThree(n: Int): Boolean {
+    // 使用循环
+//    var temp = n
+//    if (n > 1) {
+//        while (temp % 3 == 0) {
+//            temp /= 3
+//        }
+//    }
+//    return temp == 1
+    //题中n的范围是-2^31 <= n <= 2^31 - 1，而在这个范围内3的最大幂是1162261467
+    return (n > 0 && 1162261467 % n == 0)
+}
+
+/**
+ * 罗马数字转整数
+ *
+ * 罗马数字包含以下七种字符: I， V， X， L，C，D 和 M。
+ * 字符          数值
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ *
+ * 例如， 罗马数字 2 写做 II，即为两个并列的 1 。12 写做XII，即为X+II。 27 写做XXVII, 即为XX+V+II。
+ * 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做IIII，而是IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为IX。
+ *
+ * 这个特殊的规则只适用于以下六种情况：
+ * I 可以放在 V(5) 和 X(10) 的左边，来表示 4 和 9。
+ * X 可以放在 L(50) 和 C(100) 的左边，来表示 40 和 90。
+ * C 可以放在 D(500) 和 M(1000) 的左边，来表示 400 和 900。
+ * 给定一个罗马数字，将其转换成整数。
+ *
+ * @see <a href="https://leetcode.cn/leetbook/read/top-interview-questions-easy/xn4n7c/">罗马数字转整数</a>
+ */
+fun romanToInt(s: String): Int {
+    val map = HashMap<String, Int>()
+    map["I"] = 1
+    map["IV"] = 4
+    map["V"] = 5
+    map["IX"] = 9
+    map["X"] = 10
+    map["XL"] = 40
+    map["L"] = 50
+    map["XC"] = 90
+    map["C"] = 100
+    map["CD"] = 400
+    map["D"] = 500
+    map["CM"] = 900
+    map["M"] = 1000
+    var res = 0
+    var i = 0
+    while (i in 0 until s.length) {
+        if (i + 1 < s.length && map.containsKey(s.substring(i, i + 2))) {
+            res += map[s.substring(i, i + 2)]!!
+            i += 2
+        } else {
+            res += map[s.substring(i, i + 1)]!!
+            i++
+        }
+    }
+    return res
 }
